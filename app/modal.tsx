fb88fac7fd87747d9c8db0d44ec28b206c55646a
ElijahWagner007 <elijahnwagner@gmail.com>
2024-11-02 10:44:50 -0400

    three tab switch

--- app/modal.tsx
+++ app/modal.tsx
@@ -7,9 +7,13 @@ import { Text, View } from '@/components/Themed';
 export default function ModalScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
+      <Text style={styles.title}>SumoApp Info</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/modal.tsx" />
+      <Text style={styles.text}>Use Controller Tab to control SumoBot</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text style={styles.text}>Use the Settings Tab to manipulate motor control</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text style={styles.text}>Use the Edit Tab to edit your Controller Layout</Text>
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
@@ -31,5 +35,9 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  text: {
+    fontSize: 10,
+    fontWeight: 'regular',
   },
 });
